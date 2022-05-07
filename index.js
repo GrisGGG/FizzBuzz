55f475576eb778511ce74acdb907bc29e01d9861
GrisGGG <GrisGGG>
2022-05-07 14:19:11 -0500

    9. se eliminaron archivos app.js que ya no sirve

--- index.js
+++
@@ -1,8 +0,0 @@
-// Clase anterior con la que obtenemos los explorers
-const Reader = require("./lib/utils/Reader");
-const explorers = Reader.readJsonFile("explorers.json");
-
-// Aplicación del ExplorerService sobre la lista de explorers
-ExplorerService.filterByMission(explorers, "node");
-ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-ExplorerService.getExplorersUsernamesByMission(explorers, "node");
