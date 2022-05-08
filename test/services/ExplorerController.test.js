cf92ca71b608c0bca2b100c6256c03d724f2236b
GrisGGG <GrisGGG>
2022-05-07 21:28:01 -0500

    18. Se añadio applyValidationInNumber

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -1,4 +1,5 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
+const FizzbuzzService = require("./../../lib/services/FizzBuzzService");
 
 describe("Tests para ExplorerController", () => {
     test ("1) Validación getExplorersByMission", () =>{
@@ -9,8 +10,24 @@ describe("Tests para ExplorerController", () => {
         const validacion1 = ExplorerController.getExplorersUsernamesByMission("node");
         expect(validacion1.length).toBe(10);
     });
-    test ("3) Validación getExplorersUsernamesByMission", () =>{
+    test ("3) Validación getExplorersAmonutByMission", () =>{
         const validacion1 = ExplorerController.getExplorersAmonutByMission("node");
         expect(validacion1).toBe(10);
+    });
+    test ("4) Validación getFizzBuzzValidation", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(1);
+        expect(validation).toBe(1);
+    });
+    test ("5) Validación getFizzBuzzValidation", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(3);
+        expect(validation).toBe("FIZZ");
+    });
+    test ("6) Validación getFizzBuzzValidation", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(5);
+        expect(validation).toBe("BUZZ");
+    });
+    test ("7) Validación getFizzBuzzValidation", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(15);
+        expect(validation).toBe("FIZZBUZZ");
     });
 });
--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -12,14 +12,34 @@ describe("Test de Clase FizzbuzzService", () => {
         const validation = FizzbuzzService.applyValidationInExplorer(explorer3);
         expect(validation.trick).toBe("FIZZ");
     });
-    test ("5) Test explorer 2, trick = BUZZ", () =>{
-        const explorer5 = {name: "Explorer2", score: 5};
+    test ("3) Test explorer 5, trick = BUZZ", () =>{
+        const explorer5 = {name: "Explorer5", score: 5};
         const validation = FizzbuzzService.applyValidationInExplorer(explorer5);
         expect(validation.trick).toBe("BUZZ");
     });
-    test ("15) Test explorer 2, trick = FIZZBUZZ", () =>{
-        const explorer15 = {name: "Explorer2", score: 15};
+    test ("4) Test explorer 15, trick = FIZZBUZZ", () =>{
+        const explorer15 = {name: "Explorer15", score: 15};
         const validation = FizzbuzzService.applyValidationInExplorer(explorer15);
         expect(validation.trick).toBe("FIZZBUZZ");
     });
+    test ("5) Test de la Clase applyValidationInNumber", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(1);
+        expect(validation).toBe(1);
+    });
+    test ("6) Test de la Clase applyValidationInNumber", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(3);
+        expect(validation).toBe("FIZZ");
+    });
+    test ("7) Test de la Clase applyValidationInNumber", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(5);
+        expect(validation).toBe("BUZZ");
+    });
+    test ("8) Test de la Clase applyValidationInNumber", () =>{
+        const validation = FizzbuzzService.applyValidationInNumber(15);
+        expect(validation).toBe("FIZZBUZZ");
+    });
+    
 });
+
+
+
